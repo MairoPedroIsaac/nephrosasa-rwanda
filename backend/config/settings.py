01366bbe2614cea3f338882a2b7333af556f813b
MairoPedroIsaac <mairopedroisaac@gmail.com>
2025-12-29 09:04:13 +0200

    Use SQLite for MVP - PostgreSQL later

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -79,12 +79,8 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME', 'healthvault_rwanda'),
-        'USER': os.getenv('DB_USER', 'postgres'),
-        'PASSWORD': os.getenv('DB_PASSWORD', ''),
-        'HOST': os.getenv('DB_HOST', 'localhost'),
-        'PORT': os.getenv('DB_PORT', '5432'),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
